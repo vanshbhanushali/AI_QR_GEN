2863ffc4046a7934873202c9f5054b27c2fbf3aa
vanssh <vanshkbhanushali@gmail.com>
2025-10-25 23:27:09 +0530

    new updated build files and vercel json file

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Install dependencies using pip3
-pip3 install -r requirements.txt
+# Install dependencies using python3.10's pip module
+python3.10 -m pip install -r requirements.txt 
 
-# Run database migrations using python3.9
-python3.9 manage.py migrate
+# Run database migrations using python3.10
+python3.10 manage.py migrate
 
-# Collect static files using python3.9
-python3.9 manage.py collectstatic --noinput --clear
+# Collect static files using python3.10
+python3.10 manage.py collectstatic --noinput --clear
